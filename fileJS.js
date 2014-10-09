5d43b6f1f3e265025239c9e1baa85dbd7e720549
Scott Gavin <sggtgb@mail.missouri.edu>
2014-10-09 08:52:52 -0500

    Initial Import
    just playing around with highlight.  What i want is to be able to drag a
    file into that drop area and then load their text content into that box
    and highlight it with highlight.js

---
+++ fileJS.js
@@ -0,0 +1,20 @@
+<SCRIPT LANGUAGE="Javascript">
+	function readTextFile(file)
+	{
+		var rawFile = new XMLHttpRequest();
+		rawFile.open("GET", file, false);
+		rawFile.onreadystatechange = function ()
+		{
+			if(rawFile.readyState === 4)
+			{
+				if(rawFile.status === 200 || rawFile.status == 0)
+				{
+					var allText = rawFile.responseText;
+					alert(allText);
+				}
+			}
+		}
+	}
+	
+	
+</SCRIPT>
